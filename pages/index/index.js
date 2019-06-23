996a7747f580f899cf892715005d83821ed15894
taoxf <taoxf@ipharmacare.net>
2019-06-23 17:04:19 +0800

    bilibili 小程序

---
+++ pages/index/index.js
@@ -0,0 +1,128 @@
+//Page Object
+Page({
+    data: {
+        // 被点击的首页导航的菜单的索引
+        currentIndexNav:0,
+        // 首页导航数据
+        navList: [],
+        // 轮播图列表
+        swiperList: [],
+        // 视频列表
+        videosList: []
+    },
+
+    //点击首页导航按钮
+    activeNav(e) {
+        this.setData({
+            currentIndexNav:e.target.dataset.index
+        })
+    },
+
+    /**
+     * 获取首页导航数据
+     */
+    getNavList() {
+        let that = this;
+        // 利用小程序内置发送请求的方法
+        wx.request({
+            url: "https://easy-mock.com/mock/5c1dfd98e8bfa547414a5278/bili/navList",
+            success(res) {
+                // console.log(res);
+                 if (res.data.code === 0) {
+                    that.setData({
+                        navList:res.data.data.navList
+                     })
+                 }
+            }
+        })
+    },
+
+    /**
+     * 获取轮播图数据
+     */
+    getSwiperList() {
+        let that = this;
+        wx.request({
+            url: 'https://easy-mock.com/mock/5c1dfd98e8bfa547414a5278/bili/swiperList',
+            success(res) {
+                // console.log(res);
+                if(res.data.code === 0) {
+                    that.setData({
+                        swiperList: res.data.data.swiperList
+                    })
+                }
+            }
+        })
+    },
+
+    /**
+     * 获取视频列表
+     */
+    getVideosList() {
+        let that = this;
+        wx.request({
+            url: 'https://easy-mock.com/mock/5c1dfd98e8bfa547414a5278/bili/videosList',
+            success(res) {
+                // console.log(res);
+                if(res.data.code === 0) {
+                    that.setData({
+                        videosList: res.data.data.videosList
+                    })
+                }
+            }
+        })
+    },
+
+    /**
+     * 生命周期函数 - 监听页面加载
+     * @param {*} options 
+     */
+    onLoad: function (options) {
+        //1、获取首页导航数据
+        this.getNavList();
+        //2、获取轮播图数据
+        this.getSwiperList();
+        //3、获取视频数据
+        this.getVideosList();
+    },
+
+    /**
+     * 生命周期函数 - 监听页面初次渲染完成
+     */
+    onReady: function () {
+
+    },
+
+    /**
+     * 生命周期函数 - 监听页面显示
+     */
+    onShow: function () {
+
+    },
+
+    /**
+     * 生命周期函数 - 监听页面隐藏
+     */
+    onHide: function () {
+
+    },
+    onUnload: function () {
+
+    },
+    onPullDownRefresh: function () {
+
+    },
+    onReachBottom: function () {
+
+    },
+    onShareAppMessage: function () {
+
+    },
+    onPageScroll: function () {
+
+    },
+    //item(index,pagePath,text)
+    onTabItemTap: function (item) {
+
+    }
+});
